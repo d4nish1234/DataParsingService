c818dd385390fe4d527521c03c67101e0bd98ab3
Danish Mahboob <39911779+d4nish1234@users.noreply.github.com>
2024-06-12 23:49:30 -0400

    feat(vehicles): add api delay

--- src/ParserService/Vehicles/index.ts
+++ src/ParserService/Vehicles/index.ts
@@ -4,7 +4,8 @@ var { Vehicle } = require('../../graphql/Vehicle/index');
 var { VehicleType } = require('../../graphql/VehicleType/index');
 var IVehicleMake = require('./models');
 const { Sequelize, DataTypes } = require('sequelize');
-var { transformGetVehicleMakesList, transformGetMakeInfo, transformGetVehicleTypes, transformGetVehicleType } = require('./transforms');
+// const { getFewMakesData, getVehicleType } = require('../../../tests/ParserService/getTestData'); // FOR TESTS
+var { transformGetVehicleMakesList, transformGetMakeInfo, transformGetVehicleTypes, transformGetVehicleType, getVehicleMakesCount } = require('./transforms');
 
 export const getAllVehicleMakesAndTypes = async () => {
 
@@ -37,28 +38,37 @@ export const getAllVehicleMakesAndTypes = async () => {
         }
     }
 
-    var rawVehiclesData = await getMakesAPI();
-
-    // var rawVehiclesData = await getFewMakesData(); // TODO: replace with await getMakesAPI();
+    var rawVehiclesData = await getMakesAPI(); // PART 1 LIVE
+    // var rawVehiclesData = await getFewMakesData(); // PART 1 TEST
+    var vehicleCount = getVehicleMakesCount(rawVehiclesData);
+    console.log("total vehicles count: " + vehicleCount)
     var vehicleTransforms = transformGetVehicleMakesList(rawVehiclesData).map((vehicle: any) => {
         const vehicleMakeInfo: typeof IVehicleMake = transformGetMakeInfo(vehicle);
         return vehicleMakeInfo;
     });
 
     var transformedVehicles: typeof Vehicle[] = new Array<typeof Vehicle>();
-    await Promise.all(vehicleTransforms.map(async (vehicle: any) => {
-        // var vehicleTypeRaw = await getVehicleType(vehicle.Make_ID) // TEST 
-        var vehicleTypeRaw = await getVehicleTypeAPI(vehicle.Make_ID) // LIVE
-        var transformedVehicleTypes = transformGetVehicleTypes(vehicleTypeRaw)
+
+    // var vehicleTypeRaw = await getVehicleType("1") // PART 2 TEST 
+
+    var interval = 200; // how much time should the delay between two iterations be (in milliseconds)?
+    for await (const vehicle of vehicleTransforms) {
+
+        await new Promise(resolve => setTimeout(resolve, interval));
+
+        var vehicleTypeRaw = await getVehicleTypeAPI(vehicle.Make_ID) // PART 2 LIVE
+        var transformedVehicleTypes = await transformGetVehicleTypes(vehicleTypeRaw)
 
         var vehicleTypes: typeof VehicleType[] = new Array<typeof VehicleType>();
-        transformedVehicleTypes.forEach((vehicleType: any) => {
-            var transformedVehicleType = transformGetVehicleType(vehicleType);
-            var vehicleTypeDTO = new VehicleType(transformedVehicleType.VehicleTypeId, transformedVehicleType.VehicleTypeName);
-            vehicleTypes.push(vehicleTypeDTO);
-        });
+        if (transformedVehicleTypes) {
+            transformedVehicleTypes.forEach((vehicleType: any) => {
+                var transformedVehicleType = transformGetVehicleType(vehicleType);
+                var vehicleTypeDTO = new VehicleType(transformedVehicleType.VehicleTypeId, transformedVehicleType.VehicleTypeName);
+                vehicleTypes.push(vehicleTypeDTO);
+            });
+        }
         transformedVehicles.push(new Vehicle(vehicleTypes, Number(vehicle.Make_ID), vehicle.Make_Name))
-    }))
+    }
 
     // persistence
     // should be in its own folder structure
